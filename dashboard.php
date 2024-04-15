ba31fbb3ea6122fde355e7cd31f3b59b4ef564aa
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-04-15 15:16:23 +0800

    revisi

--- dashboard.php
+++ dashboard.php
@@ -17,22 +17,63 @@ if (isset($_POST["logout"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
 
-    <?php include("layouts/header.html"); ?>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <h1>Haaloo selamat datang <?= $_SESSION["username"] ?> </h1>
 
-    <h1>Haaloo selamat datang <?= $_SESSION["username"] ?> </h1>
+
+            <form action="dashboard.php" method="POST">
+                <button type="submit" name="logout" class="btn btn-secondary">Logout</button>
+            </form>
+        </div>
+
+        <input type="submit" name="create" class="btn btn-primary my-2" value="create">
+
+        <div class="card">
+            <table class=" w-full table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
 
 
-    <form action="dashboard.php" method="POST">
-        <button type="submit" name="logout">Logout</button>
-    </form>
 
-    <?php include("layouts/footer.html"); ?>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
